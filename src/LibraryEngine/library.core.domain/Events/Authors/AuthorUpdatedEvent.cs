cbd98a57d77dbc9e6e5171020a36c61ddcb91dba
giomen601 <81644638+giomen601@users.noreply.github.com>
2025-08-13 14:28:54 -0500

    author updated event

---
+++ src/LibraryEngine/library.core.domain/Events/Authors/AuthorUpdatedEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace library.core.domain.Events.Authors;
+public class AuthorUpdatedEvent : BaseEvent
+{
+  public AuthorUpdatedEvent() : base(nameof(AuthorUpdatedEvent))
+  {
+  }
+
+  public required string AuthorFirstName { get; set; }
+  public required string AuthorLastName { get; set; }
+  public required int AuthorPhone { get; set; }
+}
