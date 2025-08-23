01abac87b0e1a7359ab0464d8fb1341ec07518cf
giomen601 <81644638+giomen601@users.noreply.github.com>
2025-08-23 13:02:14 -0500

    book events

---
+++ src/LibraryEngine/library.core.domain/Events/Books/BookCreatedEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace library.core.domain.Events.Books;
+public class BookCreatedEvent : BaseEvent
+{
+  public BookCreatedEvent()
+  : base(nameof(BookCreatedEvent))
+  {
+  }
+
+  public required string Title { get; set; }
+  public required string Description { get; set; }
+  public required DateTime CreationDate { get; set; }
+}
