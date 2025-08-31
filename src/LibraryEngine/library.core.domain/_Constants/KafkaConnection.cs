a4f12aeb94b8140c74e3302e9efbfeb7584c1fff
giomen601 <81644638+giomen601@users.noreply.github.com>
2025-08-30 22:19:36 -0500

    kafka constants

---
+++ src/LibraryEngine/library.core.domain/_Constants/KafkaConnection.cs
@@ -0,0 +1,8 @@
+﻿namespace library.core.domain._Constants;
+public static class KafkaConnection
+{
+  public const string Kafka_HostName = "localhost";
+  public const string Kafka_Port = "29092";
+  public const string Author_Topic = "AUTHOR_TOPIC";
+  public const string Book_Topic = "BOOK_TOPIC";
+}
