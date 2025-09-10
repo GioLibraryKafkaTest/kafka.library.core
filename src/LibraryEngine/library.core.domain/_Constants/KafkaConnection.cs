1f4ec6be73388b00c97649f1033b8c952e83af7f
giomen601 <81644638+giomen601@users.noreply.github.com>
2025-09-10 14:55:10 -0500

    events arrays

--- src/LibraryEngine/library.core.domain/_Constants/KafkaConnection.cs
+++ src/LibraryEngine/library.core.domain/_Constants/KafkaConnection.cs
@@ -5,4 +5,7 @@ public static class KafkaConnection
   public const string Kafka_Port = "29092";
   public const string Author_Topic = "AUTHOR_TOPIC";
   public const string Book_Topic = "BOOK_TOPIC";
+
+  public static readonly string[] Author_Topics = { Author_Topic, Book_Topic };
+  public static readonly string[] Book_Topics = { Book_Topic, Author_Topic };
 }
